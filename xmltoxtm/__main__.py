6d67e738972ef5876b1a8e852baab1850419681f
Ryan O'Rourke <ryan.orourke@welocalize.com>
2020-06-15 16:41:39 -0400

    updating to version 2.0

--- xmltoxtm/__main__.py
+++ xmltoxtm/__main__.py
@@ -1,27 +1,29 @@
 import sys
 
-from xmltoxtm.functions import resource, unsource, BookInfo, get_book_info_from_zip, get_zip
+from xmltoxtm.functions import resource, unsource, BookInfo, get_book_info, get_zip, FileName
 
 
-def resource_cli(target_file: str) -> str:
+def resource_cli(target_fname: FileName) -> FileName:
     """
     This function restores the XML source files to their original structure, as
     well as restores any files that had been removed from scope during prep.
-    :param target_file: Name of target .ZIP package. Will be processed in current working directory.
+    :param target_fname: Name of target .ZIP package. This file will
+     be processed in current working directory.
     :return target_file: Name of target restructured .ZIP package.
     """
-    book = BookInfo(**get_book_info_from_zip(target_file))
+    book = BookInfo(**get_book_info(target_fname))
     course = book.invpartnumber
     source_file = get_zip(course)
-    return resource(source_file, target_file)
+    return resource(source_file, target_fname)
 
 
-def unsource_cli(course: str) -> str:
+def unsource_cli(course: str) -> FileName:
     """
     This function reorganizes the XML source files so that XTM will parse them
     in the same order as they appear in the published PDF.
     :param course: Course number to be prepped, e.g. RH124, CL310, DO180.
-    :return zip_filename: Name of restructured .ZIP package ready to be uploaded to XTM for analysis.
+    :return zip_filename: Name of restructured .ZIP package that is
+    ready to be uploaded to XTM for analysis.
     """
     source_file = get_zip(course)
     return unsource(source_file)
@@ -29,8 +31,8 @@ def unsource_cli(course: str) -> str:
 
 if __name__ == "__main__":
     if sys.argv[1].lower() == 'resource':
-        target_file = sys.argv[2]
+        target_file = FileName(sys.argv[2])
         resource_cli(target_file)
     if sys.argv[1].lower() == 'unsource':
-        course = sys.argv[2]
-        unsource_cli(course)
+        course_name = sys.argv[2]
+        unsource_cli(course_name)
--- xmltoxtm/functions.py
+++ xmltoxtm/functions.py
@@ -4,8 +4,9 @@
 import os
 import shutil
 import zipfile
+from collections import OrderedDict
 from subprocess import Popen, DEVNULL
-from typing import List, Tuple, Optional, Union, Iterable
+from typing import List, Tuple, Optional, Union, Iterable, Any, NewType
 
 import requests
 import xmltodict
@@ -13,44 +14,65 @@ from github import Github
 from lxml import etree
 from pydantic import BaseModel
 
-base_url = 'https://api.github.com'
-token = os.environ.get('github_token')
-headers = {'Authorization': f"token {token}"}
-token_url = f"https://{token}@api.github.com"
+GITHUB_TOKEN = os.environ.get('github_token')
+HEADERS = {'Authorization': f"token {GITHUB_TOKEN}"}
+
+BookStr = NewType('BookStr', Optional[Union[str, dict]])
+BookInt = NewType('BookInt', Optional[Union[int, dict]])
+FileName = NewType('FileName', str)
+FilePath = NewType('FilePath', str)
+Directory = NewType('Directory', str)
+FileList = NewType(
+    'FileList',
+    Iterable[
+        Union[
+            Tuple[FileName, FileName],
+            Tuple[FilePath, FilePath],
+            FileName,
+            FilePath
+        ]
+    ]
+)
 
 
-def get_book_info(book_info_file: str = 'Book_Info.xml'):
-    with open(book_info_file, 'r') as xml_file:
-        return xmltodict.parse(xml_file.read()).get('bookinfo')
-
-
-def get_book_info_from_zip(zipf: str, path: str = '00-introduction/01-Book_Info.xml'):
-    with zipfile.ZipFile(zipf, 'r') as f:
-        book_info_file = f.open(os.path.join(f"{f.namelist()[0].split('/', 1)[0]}", path), 'r')
+def get_book_info(zip_fname: FileName,
+                  path: str = '00-introduction/01-Book_Info.xml'
+                  ) -> OrderedDict:
+    with zipfile.ZipFile(zip_fname, 'r') as f_zip:
+        book_info_file = f_zip.open(
+            os.path.join(
+                f"{f_zip.namelist()[0].split('/', 1)[0]}",
+                path
+            ), 'r'
+        )
         return xmltodict.parse(book_info_file.read()).get('bookinfo')
 
 
-def zipdir(path, ziph):
+def zipdir(path: Directory,
+           f_zip: zipfile.ZipFile
+           ) -> None:
     for root, _, files in os.walk(path):
         for file in files:
-            ziph.write(os.path.join(root, file))
+            f_zip.write(os.path.join(root, file))
 
 
-def lists_to_tuple(*args):
+def lists_to_tuple(*args: Union[list, FileList]) -> tuple:
     new_list = []
     for arg in args:
         new_list = new_list + arg
     return tuple(new_list)
 
 
-def ppxml(path):
+def ppxml(path: Directory) -> None:
     for root, _, files in os.walk(path):
         for file in files:
             file = os.path.join(root, file)
             cmd1 = f'mv "{file}" "{file}.bak" 2>&1'
-            cmd2 = f'xmllint --format "{file}.bak" > "{file}" 2>&1'
+            cmd2 = f'xmllint --format --recover "{file}.bak" > "{file}" 2>&1'
             cmd3 = f'rm -f "{file}.bak" 2>&1'
-            final = Popen(f"{cmd1}; {cmd2}; {cmd3}", shell=True, stdin=DEVNULL,
+            pattern = r'/^\..*\.xml\.bak.*parser error.*not defined$/,+2d'
+            cmd4 = f'sed "{pattern}" -i {file}'
+            final = Popen(f"{cmd1}; {cmd2}; {cmd3}; {cmd4}", shell=True, stdin=DEVNULL,
                           stdout=DEVNULL, stderr=DEVNULL, close_fds=True)
             final.communicate()
 
@@ -71,19 +93,18 @@ SUBTITLE_INDEX = {
 
 
 class BookInfo(BaseModel):
-    productname: str
-    edition: int
-    invpartnumber: str
-    productname: str
-    productnumber: str
-    pubdate: Optional[str] = None
-    pubsnumber: Optional[str] = None
-    subtitle: Union[str, dict]
-    title: str
-    target: Optional[str] = None
+    productname: BookStr = None
+    edition: BookInt = None
+    invpartnumber: BookStr = None
+    productnumber: BookStr = None
+    pubdate: BookStr = None
+    pubsnumber: BookStr = None
+    subtitle: BookStr = None
+    title: BookStr = None
+    target: BookStr = None
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **info: Any):
+        super().__init__(**info)
         if self.pubsnumber is None:
             if self.pubdate is not None:
                 object.__setattr__(self, 'pubsnumber', self.pubdate)
@@ -92,73 +113,146 @@ class BookInfo(BaseModel):
         object.__setattr__(self, 'target', SUBTITLE_INDEX[self.subtitle])
 
 
-def get_intro(xml_file: str, source_dir: str = None, remaining_files: list = None, working_list: list = None):
+def get_intro_names(fname: Union[FileName, FilePath],
+                    source_dir: Directory = None,
+                    remaining_files: List[FileName] = None,
+                    working_list: List[FileName] = None
+                    ) -> FileList:
     remaining_files = remaining_files or []
     working_list = working_list or []
-    source_dir = source_dir or os.path.dirname(xml_file)
+    source_dir = source_dir or os.path.dirname(fname)
     parser = etree.XMLParser(recover=True)
-    root_file = xml_file if os.path.exists(xml_file) else os.path.join(source_dir, xml_file)
-    root = etree.parse(root_file, parser=parser).getroot()
-    intro_files = [x for child in root if (x := child.attrib.get('href')) if 'sg-chapters' not in x]
-    intro_files = [file for file in intro_files if os.path.exists(os.path.join(source_dir, file))]
+    root_file = fname if os.path.exists(fname) else os.path.join(source_dir, fname)
+    root = etree.parse(
+        root_file,
+        parser=parser
+    ).getroot()
+    intro_files = [
+        file
+        for child in root
+        if all([
+            (file := child.attrib.get('href')),
+            'sg-chapters' not in file,
+            os.path.exists(os.path.join(source_dir, file))
+        ])
+    ]
     new_remaining_files = intro_files + remaining_files
     new_working_list = working_list
     if intro_files:
-        if xml_file in working_list:
-            index = working_list.index(xml_file) + 1
+        if fname in working_list:
+            index = working_list.index(fname) + 1
             new_working_list[index:index] = intro_files
         else:
             new_working_list = working_list + intro_files
     if new_remaining_files:
         file, *remaining_files = new_remaining_files
-        return get_intro(file, source_dir, remaining_files, new_working_list)
-    return new_working_list
+        return get_intro_names(file, source_dir, remaining_files, new_working_list)
+    return FileList(new_working_list)
 
 
-def get_intro_file_list(intro_files: List[str], source_dir: str = None, target_dir: str = None):
-    source_dir = source_dir or '.'
+def get_intro_file_list(intro_files: FileList,
+                        target_dir: Directory = None,
+                        source_dir: Directory = None
+                        ) -> FileList:
     target_dir = target_dir or '.'
-    return [
-        (os.path.join(source_dir, file),
-         os.path.join(target_dir, f"00-introduction/{i:02d}-{f'/{i:02d}-'.join(file.split('/'))}"))
-        for i, file in enumerate(
-            intro_files, start=1)
-    ]
+    source_dir = source_dir or '.'
+    return FileList([
+        (
+            FilePath(os.path.join(
+                source_dir,
+                file
+            )),
+            FilePath(os.path.join(
+                target_dir,
+                '00-introduction',
+                f'{i:02d}'.join(file.split('/'))
+            ))
+        )
+        for i, file in enumerate(intro_files, start=1)
+    ])
 
 
-def get_chapters(xml_file):
+def get_chapter_names(fname: Union[FileName, FilePath]) -> FileList:
     parser = etree.XMLParser(recover=True)
-    root = etree.parse(xml_file, parser=parser).getroot()
-    return [x for child in root if (x := child.attrib.get('href')) if 'sg-chapters' in x]
+    root = etree.parse(
+        fname,
+        parser=parser
+    ).getroot()
+    return FileList([
+        chapter
+        for child in root
+        if all([
+            (chapter := child.attrib.get('href')),
+            'sg-chapters' in chapter
+        ])
+    ])
 
 
-def get_chapter_file_list(chapter_list: List[str], source_dir: str = None, target_dir: str = None):
-    source_dir = source_dir or '.'
+def get_chapter_file_list(chapter_list: FileList,
+                          target_dir: Directory = None,
+                          source_dir: Directory = None
+                          ) -> FileList:
     target_dir = target_dir or '.'
+    source_dir = source_dir or '.'
     chapter_file_list = []
     for i, chapter in enumerate(chapter_list, start=1):
-        chapter_dir = os.path.join(target_dir, f"{i:02d}-{os.path.basename(chapter).split('.', 1)[0]}/sg-chapters")
+        chapter_dir = os.path.join(
+            target_dir,
+            f"{i:02d}-{os.path.basename(chapter).split('.', 1)[0]}",
+            'sg-chapters'
+        )
         chapter_file_list.append(
-            (os.path.join(source_dir, chapter), os.path.join(chapter_dir, f"{i:02d}-{os.path.basename(chapter)}")))
+            (
+                os.path.join(
+                    source_dir,
+                    chapter
+                ),
+                os.path.join(
+                    chapter_dir,
+                    f"{i:02d}-{os.path.basename(chapter)}"
+                )
+            )
+        )
         parser = etree.XMLParser(recover=True)
-        root = etree.parse(os.path.join(source_dir, chapter), parser=parser).getroot()
-        sections = [x for child in root if (x := child.attrib.get('href'))]
+        root = etree.parse(
+            os.path.join(source_dir, chapter),
+            parser=parser
+        ).getroot()
+        sections = [
+            section
+            for child in root
+            if (
+                    (section := child.attrib.get('href')) is not None
+            )
+        ]
         for j, section in enumerate(sections, start=1):
             chapter_file_list.append(
-                (os.path.join(source_dir, 'sg-chapters', section),
-                 os.path.join(chapter_dir, os.path.dirname(section), f"{j:02d}-{os.path.basename(section)}"))
+                (
+                    FilePath(os.path.join(
+                        source_dir,
+                        'sg-chapters',
+                        section
+                    )),
+                    FilePath(os.path.join(
+                        chapter_dir,
+                        os.path.dirname(section),
+                        f"{j:02d}-{os.path.basename(section)}"
+                    ))
+                )
             )
-    return chapter_file_list
+    return FileList(chapter_file_list)
 
 
-def copy_and_rename(file_list: Iterable[Tuple[str, str]], reverse: bool = False):
+def copy_and_rename(file_list: FileList,
+                    reverse: bool = False
+                    ) -> None:
     if reverse:
-        new_dirs = {os.path.dirname(old) for old, new in file_list}
+        dossiers = {os.path.dirname(old) for old, new in file_list}
     else:
-        new_dirs = {os.path.dirname(new) for old, new in file_list}
-    for directory in new_dirs:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        dossiers = {os.path.dirname(new) for old, new in file_list}
+    for dossier in dossiers:
+        if not os.path.exists(dossier):
+            os.makedirs(dossier)
     for old, new in file_list:
         if reverse:
             shutil.copy(new, old)
@@ -166,80 +260,131 @@ def copy_and_rename(file_list: Iterable[Tuple[str, str]], reverse: bool = False)
             shutil.copy(old, new)
 
 
-def unsource(source_zip):
-    with zipfile.ZipFile(source_zip, 'r') as zipf:
-        zip_dir = os.path.join(zipf.namelist()[0].split('/', 1)[0], 'guides/en-US')
-        for file in zipf.namelist():
+def unsource(source_fname: FileName) -> FileName:
+    with zipfile.ZipFile(source_fname, 'r') as f_zip:
+        zip_dir = os.path.join(
+            f_zip.namelist()[0].split('/', 1)[0],
+            'guides/en-US'
+        )
+        for file in f_zip.namelist():
             if zip_dir in file:
-                zipf.extract(file)
+                f_zip.extract(file)
         shutil.move(zip_dir, '.')
-        shutil.rmtree(zipf.namelist()[0].split('/', 1)[0])
-    book = BookInfo(**get_book_info_from_zip(source_zip, 'guides/en-US/Book_Info.xml'))
-    os.remove(source_zip)
+        shutil.rmtree(f_zip.namelist()[0].split('/', 1)[0])
+    book = BookInfo(
+        **get_book_info(
+            source_fname,
+            'guides/en-US/Book_Info.xml'
+        )
+    )
+    os.remove(source_fname)
     os.chdir('./en-US')
-    source_dir = None
-    target_dir = './en-US'
+    target_dir = Directory('./en-US')
     os.mkdir(target_dir)
-    toc = f"./{book.invpartnumber}-SG.xml"
+    toc = FilePath(f"./{book.invpartnumber}-SG.xml")
     intro_file_list = get_intro_file_list(
-        get_intro(toc), source_dir, target_dir
+        get_intro_names(toc), target_dir
     )
     chapter_file_list = get_chapter_file_list(
-        get_chapters(toc), source_dir, target_dir
+        get_chapter_names(toc), target_dir
     )
     file_list = lists_to_tuple(intro_file_list, chapter_file_list)
     copy_and_rename(file_list)
-    zip_filename = f"{book.invpartnumber}-{book.pubsnumber}.zip"
-    zipf = zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED)
-    zipdir(target_dir, zipf)
-    shutil.move(zip_filename, '..')
+    zip_fname = FileName(f"{book.invpartnumber}-{book.pubsnumber}.zip")
+    with zipfile.ZipFile(zip_fname, 'w', zipfile.ZIP_DEFLATED) as f_zip:
+        zipdir(target_dir, f_zip)
+    shutil.move(zip_fname, '..')
     os.chdir('..')
     shutil.rmtree(target_dir)
-    return zip_filename
+    return zip_fname
 
 
-def resource(source_file, target_file):
-    with zipfile.ZipFile(source_file, 'r') as zipf:
-        source_dir = os.path.join(zipf.namelist()[0].split('/', 1)[0], 'guides')
-        for file in zipf.namelist():
+def resource(source_fname: FileName,
+             target_fname: FileName
+             ) -> FileName:
+    with zipfile.ZipFile(source_fname, 'r') as f_zip:
+        source_dir = os.path.join(
+            f_zip.namelist()[0].split('/', 1)[0],
+            'guides'
+        )
+        for file in f_zip.namelist():
             if source_dir in file:
-                zipf.extract(file)
-    with zipfile.ZipFile(target_file, 'r') as zipf:
-        zipf.extractall()
+                f_zip.extract(file)
+    with zipfile.ZipFile(target_fname, 'r') as f_zip:
+        f_zip.extractall()
     source_dir = os.path.join(source_dir, 'en-US')
-    target_dir = './en-US'
+    target_dir = Directory('./en-US')
     ppxml(target_dir)
-    book = BookInfo(**get_book_info(os.path.join(target_dir, '00-introduction/01-Book_Info.xml')))
-    toc = os.path.join(source_dir, f"{book.invpartnumber}-SG.xml")
-    intro_file_list = get_intro_file_list(get_intro(toc), os.path.join('.', source_dir), target_dir)
-    chapter_file_list = get_chapter_file_list(get_chapters(toc), os.path.join('.', source_dir), target_dir)
+    book = BookInfo(
+        **get_book_info(
+            target_fname,
+            '00-introduction/01-Book_Info.xml'
+        )
+    )
+    toc = FilePath(os.path.join(
+        source_dir,
+        f"{book.invpartnumber}-SG.xml"
+    ))
+    intro_file_list = get_intro_file_list(
+        get_intro_names(toc),
+        target_dir,
+        Directory(os.path.join('.', source_dir))
+    )
+    chapter_file_list = get_chapter_file_list(
+        get_chapter_names(toc),
+        target_dir,
+        Directory(os.path.join('.', source_dir))
+    )
     file_list = lists_to_tuple(intro_file_list, chapter_file_list)
     copy_and_rename(file_list, reverse=True)
     shutil.rmtree(target_dir)
-    shutil.move(source_dir, os.path.join(source_dir.rsplit('/', 1)[0], book.target))
+    shutil.move(
+        source_dir,
+        os.path.join(
+            source_dir.rsplit('/', 1)[0],
+            book.target
+        )
+    )
     for target in SUBTITLE_INDEX.values():
-        if target != book.target and os.path.exists(os.path.join(source_dir.rsplit('/', 1)[0], target)):
-            shutil.rmtree(os.path.join(source_dir.rsplit('/', 1)[0], target))
-    target_filename = f"{book.invpartnumber}-{book.pubsnumber}_{book.target}.zip"
-    zipf = zipfile.ZipFile(target_filename, 'w', zipfile.ZIP_DEFLATED)
-    zipdir(source_dir.split('/')[0], zipf)
+        if all([
+            target != book.target,
+            os.path.exists(
+                os.path.join(
+                    source_dir.rsplit('/', 1)[0],
+                    target
+                )
+            )
+        ]):
+            shutil.rmtree(
+                os.path.join(
+                    source_dir.rsplit('/', 1)[0],
+                    target
+                )
+            )
+    resourced_fname = FileName(f"{book.invpartnumber}-{book.pubsnumber}_{book.target}.zip")
+    with zipfile.ZipFile(resourced_fname, 'w', zipfile.ZIP_DEFLATED) as f_zip:
+        zipdir(Directory(source_dir.split('/')[0]), f_zip)
     shutil.rmtree(source_dir.split('/')[0])
-    return target_filename
+    return resourced_fname
 
 
-def get_zip(course: str, user: str = 'RedHatTraining', token: str = token):
+def get_zip(course: str,
+            user: str = 'RedHatTraining',
+            token: str = GITHUB_TOKEN
+            ) -> FileName:
     g = Github(token)
     release = g.get_user(user).get_repo(course).get_latest_release()
-    zipball = requests.get(release.zipball_url, headers=headers, stream=True)
+    zipball = requests.get(release.zipball_url, headers=HEADERS, stream=True)
     course = release.url.split('/', 6)[5]
-    fname = f"{course}-{release.tag_name}.zip"
-    open(fname, 'wb').write(zipball.content)
+    fname = FileName(f"{course}-{release.tag_name}.zip")
+    with open(fname, 'wb') as f_zip:
+        f_zip.write(zipball.content)
     with zipfile.ZipFile(fname, 'r') as bad_zip:
         bad_dir = bad_zip.namelist()[0].split('/')[0]
         bad_zip.extractall()
     os.remove(fname)
     shutil.move(bad_dir, fname.rsplit('.', 1)[0])
-    zipf = zipfile.ZipFile(fname, 'w', zipfile.ZIP_DEFLATED)
-    zipdir(fname.rsplit('.', 1)[0], zipf)
+    with zipfile.ZipFile(fname, 'w', zipfile.ZIP_DEFLATED) as f_zip:
+        zipdir(Directory(fname.rsplit('.', 1)[0]), f_zip)
     shutil.rmtree(fname.rsplit('.', 1)[0])
     return fname
