8d66c55ee0be34dbf8960c3cba46f9230c4bfdac
Ryan O'Rourke <ryan.orourke@welocalize.com>
2020-06-15 06:31:37 -0400

    Register version 1.0.0

--- xmltoxtm/__main__.py
+++ xmltoxtm/__main__.py
@@ -10,8 +10,8 @@ def resource_cli(target_file: str) -> str:
     """
     This function restores the XML source files to their original structure, as
     well as restores any files that had been removed from scope during prep.
-    :param target_file: Name of target .ZIP file. Will be processed in current working directory.
-    :return target_file: Name of target restructured .ZIP file.
+    :param target_file: Name of target .ZIP package. Will be processed in current working directory.
+    :return target_file: Name of target restructured .ZIP package.
     """
     book = BookInfo(**get_book_info_from_zip(target_file))
     course = book.invpartnumber
@@ -25,7 +25,7 @@ def unsource_cli(course: str) -> str:
     This function reorganizes the XML source files so that XTM will parse them
     in the same order as they appear in the published PDF.
     :param course: Course number to be prepped, e.g. RH124, CL310, DO180.
-    :return zip_filename: Name of restructured .ZIP file ready to be uploaded to XTM for analysis.
+    :return zip_filename: Name of restructured .ZIP package ready to be uploaded to XTM for analysis.
     """
     release = g.get_user('RedHatTraining').get_repo(course).get_latest_release()
     source_file = get_zip(release)
