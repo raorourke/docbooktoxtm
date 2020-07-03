64156fb91d760ef4c4740be5d3c1185675a58084
Ryan O'Rourke <ryan.orourke@welocalize.com>
2020-07-03 14:24:16 -0400

    update config

--- docbooktoxtm/config.py
+++ docbooktoxtm/config.py
@@ -3,7 +3,7 @@ import os
 from subprocess import Popen, DEVNULL
 
 USE_ENVIRONMENT_VARIABLES = 1
-DEFAULT_CONFIG_FILE = '/$HOME/.weconfig/config.sh'
+DEFAULT_CONFIG_FILE = '$HOME/.weconfig/config.sh'
 GITHUB_TOKEN: str = 'GITHUB_TOKEN_PLACEHOLDER'
 
 if USE_ENVIRONMENT_VARIABLES:
